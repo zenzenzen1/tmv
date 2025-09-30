dacbbc49e3d16a6101eff6f8290a31eb1a5ea283
hieu6903 <hieu6903az@gmail.com>
2025-09-30 23:07:02 +0700

    feat(FVC-8): add common component

--- fvc-frontend/src/App.tsx
+++ fvc-frontend/src/App.tsx
@@ -1,14 +1,11 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
-  return (
-    <div className="app-container">
-      <div className="text-blue-500 text-3xl flex justify-center items-center h-screen">
-        Hello World
-      </div>
+  <div className="app-container">
+    <div className="text-blue-500 text-3xl flex justify-center items-center h-screen">
+      Hello World
     </div>
-  )
+  </div>;
 }
 
-export default App
+export default App;
---
+++ fvc-frontend/src/components/layout/sidebarMenu.ts
@@ -0,0 +1,14 @@
+export type MenuItem = {
+  key: string;
+  label: string;
+  section?: string;
+};
+
+export const defaultMenuItems: MenuItem[] = [
+  { key: "tournaments", label: "Danh sách giải đấu" },
+  { key: "tournamentForm", label: "Form đăng ký giải đấu" },
+  { key: "athletes", label: "Quản lí VDV" },
+  { section: "Quản lí nội dung", key: "fighting", label: "Đối kháng" },
+  { section: "Quản lí nội dung", key: "forms", label: "Quyền" },
+  { section: "Quản lí nội dung", key: "music", label: "Võ nhạc" },
+];
